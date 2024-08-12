308808295a06a18bd343c914b37000858de98b0e
zeke-chin <2587854353@qq.com>
2024-08-13 00:09:26 +0800

    feat: rs_db init

---
+++ src-tauri/migration/src/m20240812_155124_create_table.rs
@@ -0,0 +1,79 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Create History table
+        manager
+            .create_table(
+                Table::create()
+                    .table(History::Table)
+                    .if_not_exists()
+                    .col(ColumnDef::new(History::Id).integer().not_null().auto_increment().primary_key())
+                    .col(ColumnDef::new(History::Type).integer().not_null())
+                    .col(ColumnDef::new(History::Value).text().null())
+                    .col(ColumnDef::new(History::Search).text().null())
+                    .col(ColumnDef::new(History::Width).integer().null())
+                    .col(ColumnDef::new(History::Height).integer().null())
+                    .col(ColumnDef::new(History::Size).integer().null())
+                    .col(ColumnDef::new(History::Timestamp).integer().null())
+                    .col(ColumnDef::new(History::TagId).integer().null())
+                    .to_owned(),
+            )
+            .await?;
+
+        // Create Tags table
+        manager
+            .create_table(
+                Table::create()
+                    .table(Tags::Table)
+                    .if_not_exists()
+                    .col(ColumnDef::new(Tags::Id).integer().not_null().auto_increment().primary_key())
+                    .col(ColumnDef::new(Tags::Name).text().null())
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Drop History table
+        manager
+            .drop_table(Table::drop().table(History::Table).to_owned())
+            .await?;
+
+        // Drop Tags table
+        manager
+            .drop_table(Table::drop().table(Tags::Table).to_owned())
+            .await?;
+
+        Ok(())
+    }
+}
+
+/// Represents the "history" table
+#[derive(Iden)]
+enum History {
+    Table,
+    Id,
+    Type,
+    Value,
+    Search,
+    Width,
+    Height,
+    Size,
+    Timestamp,
+    TagId,
+}
+
+/// Represents the "tags" table
+#[derive(Iden)]
+enum Tags {
+    Table,
+    Id,
+    Name,
+}
---
+++ src-tauri/src/core/db/mod.rs
@@ -0,0 +1,3 @@
+pub mod crud;
+pub mod entities;
+pub mod connection;
